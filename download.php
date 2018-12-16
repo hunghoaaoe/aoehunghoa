5a6c8db8dfc507bdffa7ebf14c86b6ddbcb44528
hunghv62 <44781723+hunghv62@users.noreply.github.com>
2018-12-17 00:10:29 +0700

    trang download

---
+++ download.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><!--link icon-->
+	<link rel="shortcut icon" type="image/png" href="image/favicon.jpg"/>
+	<meta charset="utf-8">
+	<title>AOE TV - Xem Các Trận AOE Đỉnh Cao </title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container" style="padding-left: 0px;padding-right: 0px;">
+		<div id="header">
+			<div class="row">
+				<div class="col-sm-8">
+					<a href="index.php" title=""><img src="image/logo.jpg" class="imglogo" swidth="100" height="100" alt=""></a>
+				</div>
+				<div class="col-sm-4" style="padding-top: 50px;">
+					<div class="search-box"> 
+					    <form class="search-form" action="">
+					     <input class="form-control" placeholder="Search..." type="text"> 
+					     <button class="btn btn-link search-btn"> <i class="glyphicon glyphicon-search"></i> 
+					     </button> 
+					    </form> 
+				   	</div> 
+				</div>
+			</div>
+		</div>
+		<div class="menu">
+			<nav class="navbar navbar-default " role="navigation">
+			    <div class="container">
+			        <div class="navbar-header">
+			            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			                <span class="sr-only">menu mobile</span>
+			                <span class="icon-bar"></span>
+			                <span class="icon-bar"></span>
+			                <span class="icon-bar"></span>
+			            </button>
+			            <a class="navbar-brand" href="">Cộng Đồng AOE</a>
+			        </div>
+			        <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
+			            <ul class="nav navbar-nav">
+			                <li ><a href="index.php">TRANG CHỦ</a></li>
+			                 <li class="dropdown">
+			                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">GAME THỦ<span class="caret"></span></a>
+			                    <ul class="dropdown-menu" role="menu">
+			                        <li><a href="#">ChimSeDiNang</a></li>
+			                        <li><a href="#">BiBi</a></li>
+			                        <li><a href="#">Shenlong</a></li>
+			                        <li><a href="#">No1</a></li>
+			                    </ul>
+			                </li>
+			                <li class="dropdown">
+			                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">THỂ LOẠI<span class="caret"></span></a>
+			                    <ul class="dropdown-menu" role="menu">
+			                        <li><a href="#">Solo</a></li>
+			                        <li><a href="#">Team 2Vs2</a></li>
+			                        <li><a href="#">Team 3Vs3</a></li>
+			                    </ul>
+			                </li>
+			                <li class="active"><a href="#">DOWLOAD</a></li>
+			                <li><a href="tips.php">GUIDE</a></li>  
+			            </ul>
+			        </div>
+			    </div>
+			</nav>
+	 	</div>
+	 	<div class="main">
+	 		<h3>CẤU HÌNH TỐI THIỂU:</h3>
+	 		<p>
+			OS:Windows Vista, 7, 8 Pro+ <br>
+			Processor:1.2GHZ CPU <br>
+			Memory:1 GB RAM <br>
+			Graphics:Direct X 9.0c Capable GPU <br>
+			DirectX®:9.0c <br>
+			Hard Drive:2 GB HD space <br>
+			</p>
+	 		<h3>CÁCH CHƠI AOE ONLINE MỚI NHẤT HIỆN NAY QUA GARENA LAN HOẶC GAMETV PLUS.</h3>
+	 		<p>Hiện nay Garena Plus đã khai tử dòng game Offline và bù lại Garena cũng không phụ lòng người chơi AOE họ đã ra 1 phiên bản dành riêng cho người dùng chơi AOE. Cách chơi như sau:</p>
+	 		<p><span style="font-style: bold">Cách 1:</span> Các bạn hay vào trang <a href="http://lan.garena.com/download/">http://lan.garena.com/download/</a> Sau đó các bạn tải garena lan về rồi cài đặt chơi như Garena Hồi xưa ^^. </p>
+	 		<p><span>Cách 2.</span> Ngoài Garena các bạn còn có thể dùng GAMETV Plus  thay thế cho Garena : các bạn vào đây để tải về rồi cài đặt chơi như bình thường nhé : <a href="http://gametvplus.com/">http://gametvplus.com/</a></p>
+	 		<p>P/s: Bạn nào chưa biết cách có thể liên hệ mình qua fb or mail ở dưới nhé! .</p>
+
+
+
+	 		<h3>Age Of Empire I (aoe1) là một game thuộc thể loại chiến thuật của Microsoft sản xuất năm 1992.</h4>
+	 		<p>Sau này hãng đã sản xuất thêm 2 phiên bản của Aoe là Aoe II: Age of King và The Conquerors Expansion, và bản cuối cùng là Age Of Empire III với đồ họa 3D.
+			Tuy đã có 3 phiên bản với nhiều cải tiến về đồ họa cũng như âm thanh nhưng bản Aoe 1 vẫn được số đông người chơi aoe yêu thích vì tính đơn giản trong cách chơi của bản 1.0 này.
+			Bản này đích thân mình zip và Up lên nên không lo có virus, các bạn tải aoe1 từ vnshare.net sẽ bị virus, mình tải về thử và bị NAV thông báo.</p>
+			<p>1. AOE bản chuẩn:,</p> 
+			<p>- Đây là bản đế chế chuẩn dùng trong thi đấu (gồm 50 giọng)</p>
+			<p>Link download: <a href="http://www.fshare.vn/file/GQ4YBEQ7IRZF">http://www.fshare.vn/file/GQ4YBEQ7IRZF</a></p>
+			<p>2. File việt hóa AOE:</p>
+			<p>- Nếu bạn nào đã có bản đế chế chuẩn rồi thì chỉ cần download file việt hóa này về giải nén chép đè vào thư mục đế chế để thưởng thức.</p>
+			<p>Link download: <a href="http://www.fshare.vn/file/6E6FAVCPC8UC">http://www.fshare.vn/file/6E6FAVCPC8UC</a></p>
+			<p>3. Auto Join 5.0 Crack.</p>
+			<p>- Vào room nhanh chóng và dễ dàng, không cần đợi 5 giây <br>
+			- Tunnel cả room một cách nhanh chóng...</p>
+			<p>Link download: <a href="http://www.fshare.vn/file/O2V2B4DA26E8">http://www.fshare.vn/file/O2V2B4DA26E8</a></p>
+			<p>4. Tools tắt, mở tính năng UPnP:</p>
+			<p>- Hiện tượng khi chơi đế chế bạn không thể tìm thấy Game hoặc tìm thấy những không vào được Game đó. Có nhiều nguyên nhân dẫn đến hiện tượng này. <br>
+			- Một trong những nguyên nhân đó là do tính năng UPnP của modem đang ở chế độ bật (Enable). Khi đó bạn dùng tools dưới đây để tắt tính năng UPnP của moden.</p>
+			<p>Link download: <a href="http://www.fshare.vn/file/K16JVLZW9CO3">http://www.fshare.vn/file/K16JVLZW9CO3</a></p></p>
+			<p>6. Cách chụp ảnh màn hình game AOE - Đế Chế</p>
+			<p>- Chỉ cần nhấn tổ hợp phím Ctrl + F12 là ảnh sẽ được lưu trong thư mục của game đế chế. Các file ảnh tương ứng với số lần bạn nhấn, được đặt tên là AoExxxx.bmp (xxxx là số, bắt đầu từ 0001)</p>
+
+
+
+			<h3>Giới thiệu về Game Đế chế 2 – AOE II</h3> 
+			<p>
+			AoE 2: Rise of the Rajas – Đế Chế 2 có Việt Nam là bản mở rộng đưa thêm 4 chủng tộc vào game gồm: Burmese, Khmer, Malay và đặc biệt là Vietnamese. Ngoài việc được điều khiển các binh chủng của nước Đại Việt, game thủ nước nhà sẽ có cơ hội ôn lại những bài học lịch sử về cuộc kháng chiến chống ách đô hộ dưới thời vua Lê Thái Tổ theo góc nhìn trực quan nhất – đó là trực tiếp tham gia những trận đánh đậm tính sử thi.<br>
+
+			Trải qua nhiều phiên bản và nâng cấp về chất lượng, Age of Empires đã khẳng định được vị trí trong lòng fan hâm mộ, tiếp tục làm mưa làm gió từ các giải đấu lớn được tổ chức quy mô cho đến từng quán internet nhỏ.Một tin vui đó chính là vào ngày 10/4/2013 phiên bản HD của Age of Empires II chính thức ra mắt như một món quà dành tặng cho các game thủ đã dành tình yêu cho tựa game chiến thuật này trong suốt thời gian qua.</p>
+			<p>Link download: <a href="https://www.fshare.vn/file/ZYIGHSAROV2B">https://www.fshare.vn/file/ZYIGHSAROV2B</a></p>
+			<p style="color: green">Nguồn: Sưu Tầm </p>
+
+	 	</div>
+	 	<div class="footer">
+			<div class="row d1footer">
+				<div class="col-sm-4" style="padding-left: 40px;">
+					<h3 class="csmin">GAME THỦ NỔI BẬT</h3>
+					<p class="csmin-p"><a href="">Chim Sẻ Đi Nắng</a> • <a href="">BiBi</a></p>
+					<p class="csmin-p"><a href="">Hồng Anh</a> • <a href="">Gunny</a> • <a href="">HeHe</a></p>
+					<p class="csmin-p"><a href="">Vanelove</a> • <a href="">Hoàng Mai Nhi</a></p>
+				</div>
+				<div class="col-sm-4">
+
+					<h3 class="csmin">THỂ LOẠI &amp; CLAN</h3>
+
+					<p class="csmin-p"><a href="">Solo</a> • <a href="">2Vs2</a> • <a href="">3Vs3</a> • <a href="">4Vs4</a></p>
+
+					<p class="csmin-p"><a href="">GameTV</a> • <a href="">Hà Nội</a> • <a href="http://cohet.org/tag/thaibinh">Thái Bình</a></p>
+
+					<p class="csmin-p"><a href="">BiBi Club</a> • <a href="">Trường Phát EC</a></p>
+
+				</div>
+				
+				<div class="col-sm-4">
+					<h3 class="csmin">LIÊN HỆ ADMIN</h3>
+					<p class="csmin-p">Email: <b>hunghv62@wru.vn</b></p>
+					<p class="csmin-p">Designed by <b>hung_hoa aoe team</b></p>
+					<div style="clear:both;"></div>
+				</div>
+			</div>
+		</div>
+	 		
+	</div>
+	
+	
+
+
+</body>
+</html>
+
