70f9f1a59a0e76f1c6c792ba8d7011046092120c
hunghv62 <44781723+hunghv62@users.noreply.github.com>
2019-01-03 11:00:55 +0700

    AOE, nhóm NO32325
    file cuối cùng

--- aoe/modules/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	  $tenmaychu='localhost';
-	  $tentaikhoan='root';
-	  $pass='';
-	  $csdl='aoe';
-	  $conn=mysqli_connect($tenmaychu,$tentaikhoan,$pass,$csdl) or die('Không kết nối được');
-	  mysqli_query($conn,"SET NAMES 'UTF8'");
-?>
